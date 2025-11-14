cdf5995a1c5a8bfb8cb72fcd5bd3e6a16541d47b
Ketellyn Santos <ketellyn.santos@sptech.school>
2025-11-14 17:01:57 -0300

    Modificação de pastas

--- sql/batgirl.sql
+++ sql/batgirl.sql
@@ -11,16 +11,13 @@ create table usuario (
 
 CREATE TABLE carros (
     idCarro INT PRIMARY KEY AUTO_INCREMENT,
-    marca VARCHAR(50), -- Honda, BMW..
-    modelo VARCHAR(50), -- CIVIC, M3..
-    anoLancamento DATE,
-    valor DECIMAL(12,2)
+    nota INT,
+    fkUser INT,
+    constraint ckNota CHECK (nota in(nota >= 0 AND nota <= 10)),
+    onstraint fkUser FOREIGN KEY (fkUser) REFERENCES usuario(idUser)
 );
 
+ALTER TABLE carros ADD COLUMN fkUser INT;
+ALTER Table carros ADD constraint fkUser FOREIGN KEY (fkUser) REFERENCES usuario(idUser);
 
-
-use aquatech;
-
-show tables;
-
-desc usuario;
+desc carros;
