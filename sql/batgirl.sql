840b6b3f1fd6969e2f498bb6a100d9f9dfe3f357
Ketellyn Santos <ketellyn.santos@sptech.school>
2025-11-16 02:02:57 -0300

    ALteração em BD para iniciar o cadastro

--- sql/batgirl.sql
+++ sql/batgirl.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE batgirl;
 use batgirl;
 
+show tables;
+
 create table usuario (
     idUser INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
@@ -9,13 +11,49 @@ create table usuario (
     senha VARCHAR(50)
 );
 
+INSERT INTO usuario (nome, dtNasc, apelido, senha) VALUES
+('Kitty Keisuke','2006-08-11','Kitty','keke1020'),
+('Matheus Menino','2007-04-27','Boneca','theus123');
+
 CREATE TABLE carros (
     idCarro INT PRIMARY KEY AUTO_INCREMENT,
     nota INT,
     fkUser INT,
-    constraint ckNota CHECK (nota in(nota >= 0 AND nota <= 10)),
-    onstraint fkUser FOREIGN KEY (fkUser) REFERENCES usuario(idUser)
+    FOREIGN KEY (fkUser) REFERENCES usuario(idUser)
 );
+    --constraint ckNota CHECK (nota in (nota >= 0 AND nota <= 10)),
+
+
+
+drop table carros;
+
+
+INSERT INTO carros (nota, fkUser) VALUES 
+(7,1),
+(10,2),
+(6,3),
+(8,2),
+(3,1),
+(6,1),
+(7,1),
+(4,2);
+
+select SUM(nota) from carros;
+
+select ROUND(SUM(nota) / (select COUNT(nota) from carros),1)
+from carros;
+
+select ROUND(SUM(nota) / (select COUNT(nota) from carros),1)
+from carros
+where idCarro = 1;
+
+select Sum(carros.nota), usuario.nome 
+from carros join usuario
+on idUser = fkUser
+where fkUser = 1;
+
+
+select * from carros;
 
 ALTER TABLE carros ADD COLUMN fkUser INT;
 ALTER Table carros ADD constraint fkUser FOREIGN KEY (fkUser) REFERENCES usuario(idUser);
