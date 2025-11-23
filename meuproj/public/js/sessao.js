049868b70676137a02ae54818f89716db57b126b
Ketellyn Santos <ketellyn.santos@sptech.school>
2025-11-23 19:58:58 -0300

    ATUALIZAÇÃO - parte de sessao funcionando

--- meuproj/public/js/sessao.js
+++ meuproj/public/js/sessao.js
@@ -1,12 +1,12 @@
 // sessão
 
 function validarSessao() {
-    var email = sessionStorage.EMAIL_USUARIO;
+    var apelido = sessionStorage.APELIDO_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
 
-    if (email != null && nome != null) {
+    if (apelido != null && nome != null) {
         b_usuario.innerHTML = nome;
     } else {
         window.location = "../login.html";
